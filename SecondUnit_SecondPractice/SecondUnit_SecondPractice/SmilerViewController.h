bdd027168ee20c020cae12ac0d0e6249fac0cee8
Jose Luis Alcala Ramos <joseluis@MacBook-Pro-de-Jose.local>
2014-07-10 00:47:05 +0200

    Practice 2.2

---
+++ SecondUnit_SecondPractice/SecondUnit_SecondPractice/SmilerViewController.h
@@ -0,0 +1,13 @@
+//
+//  SmilerViewController.h
+//  SecondUnit_SecondPractice
+//
+//  Created by Jose Luis Alcala Ramos on 09/07/14.
+//  Copyright (c) 2014 Seas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SmilerViewController : UIViewController
+
+@end
---
+++ SecondUnit_SecondPractice/SecondUnit_SecondPractice/SmilerViewController.m
@@ -0,0 +1,37 @@
+//
+//  SmilerViewController.m
+//  SecondUnit_SecondPractice
+//
+//  Created by Jose Luis Alcala Ramos on 09/07/14.
+//  Copyright (c) 2014 Seas. All rights reserved.
+//
+
+#import "SmilerViewController.h"
+
+@interface SmilerViewController ()
+
+@end
+
+@implementation SmilerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
