a643cda4028c688004cf779f0cfc5682479426b8
Jose Luis Alcala Ramos <joseluis@MacBook-Pro-de-Jose.local>
2014-07-09 20:34:09 +0200

    Practice 2.1

---
+++ SecondUnit_SecondPractice/SecondUnit_SecondPractice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SecondUnit_SecondPractice
+//
+//  Created by Jose Luis Alcala Ramos on 09/07/14.
+//  Copyright (c) 2014 Seas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
