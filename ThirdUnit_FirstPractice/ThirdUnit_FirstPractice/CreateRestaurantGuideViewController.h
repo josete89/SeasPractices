8753559567febf31f5ed76ba89c0155bc2acc4d2
Jose Luis Alcala Ramos <joseluis@MacBook-Pro-de-Jose.local>
2014-07-10 17:34:13 +0200

    Practice 3.1

---
+++ ThirdUnit_FirstPractice/ThirdUnit_FirstPractice/CreateRestaurantGuideViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateRestaurantGuideViewController.h
+//  ThirdUnit_FirstPractice
+//
+//  Created by Jose Luis Alcala Ramos on 10/07/14.
+//  Copyright (c) 2014 Seas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateRestaurantGuideViewController : UIViewController
+
+@end
---
+++ ThirdUnit_FirstPractice/ThirdUnit_FirstPractice/DetailRestauranGuideViewController.m
@@ -0,0 +1,37 @@
+//
+//  DetailRestauranGuideViewController.m
+//  ThirdUnit_FirstPractice
+//
+//  Created by Jose Luis Alcala Ramos on 10/07/14.
+//  Copyright (c) 2014 Seas. All rights reserved.
+//
+
+#import "DetailRestauranGuideViewController.h"
+
+@interface DetailRestauranGuideViewController ()
+
+@end
+
+@implementation DetailRestauranGuideViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
